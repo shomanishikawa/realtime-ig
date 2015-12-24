11794d0bf41a64765400d4a4c5e7a119a937c140
Shoma Nishikawa <shoma.nishikawa@gmail.com>
2015-12-24 16:16:07 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# react-webpack-boilerplate
+
+```
+$ npm install
+$ npm start
+& ngrok http 8080
+```
+
+# instagram API
+```
+CLIENT_ID=YOUR_CLIENT_ID
+CLIENT_SECRET=YOUR_CLIENT_SECRET
+CALLBACK_URL=YOUR_NGROK_URL/gimmecats
+```
---
+++ gulpfile.babel.js
@@ -0,0 +1,32 @@
+import gulp from 'gulp'
+import browserify from 'browserify'
+import reactify from 'reactify'
+import source from 'vinyl-source-stream'
+import babelify from 'babelify'
+import uglify from 'gulp-uglify'
+
+gulp.task('browserify', () => {
+  return browserify('./src/main.js')
+    .transform(babelify)
+    .transform(reactify)
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('copy', () => {
+  gulp.src('src/index.html').pipe(gulp.dest('dist'));
+  gulp.src('src/assets/**/*.*').pipe(gulp.dest('dist/assets'));
+});
+
+gulp.task('default',['browserify', 'copy'], () => {
+  return gulp.watch('src/**/*.*', ['browserify', 'copy'])
+});
+
+gulp.task('compress', ['browserify'], () => {
+  return gulp.src('dist/js/main.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('deploy', ['copy', 'browserify', 'compress']);
